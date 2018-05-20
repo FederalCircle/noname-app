0e1f95b00faa3771cc5d9b1e196eb553781c62be
Souza <juliana.c.souza@accenture.com>
2018-05-20 07:02:14 -0300

    my name is juh

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,17 +4,17 @@ import './Form.css'
 const search = props => {
     return (
         <div className='Form'>
-            <form>
+            <form name="rotina" onSubmit={props.handleSubmit}>
                 <div className="row">
                     <h3 className="title">Horário Partida:</h3>
                     <div className="col-sm-12">
                         <div className="form-group col-side">
                             <label>De </label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="idaDe" onChange={props.handleChange} />
                         </div>
                         <div className="form-group col-side col-right">
                             <label>Até </label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="idaAte" onChange={props.handleChange} />
                         </div>
                     </div>
                 </div>
@@ -23,11 +23,11 @@ const search = props => {
                     <div className="col-sm-12">
                         <div className="form-group col-side">
                             <label>De </label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="voltaDe" onChange={props.handleChange} />
                         </div>
                         <div className="form-group col-side col-right">
                             <label>Até </label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="voltaAte" onChange={props.handleChange} />
                         </div>
                     </div>
                 </div>
@@ -36,51 +36,51 @@ const search = props => {
                     <div className="col-sm-12">
                         <div className="checkbox">
                             <div className="check-option">
-                                <label>
-                                    <input type="checkbox" className="checkOpt" />
+                                <label htmlFor="domingo">
+                                    <input type="checkbox" id="domingo" className="checkOpt" name="domingo" onChange={props.handleChange} />
                                     Domingo
                                 </label>
                             </div>
                             <div className="check-option">
-                                <label>
-                                    <input type="checkbox" className="checkOpt" />
+                                <label htmlFor="segunda">
+                                    <input type="checkbox" id="segunda" className="checkOpt" name="segunda" onChange={props.handleChange}/>
                                     Segunda
                                 </label>
                             </div>
                             <div className="check-option">
-                                <label>
-                                    <input type="checkbox" className="checkOpt" />
+                                <label htmlFor="terca">
+                                    <input type="checkbox" id="terca" className="checkOpt" name="terca" onChange={props.handleChange}/>
                                     Terça
                                 </label>
                             </div>
                             <div className="check-option">
-                                <label>
-                                    <input type="checkbox" className="checkOpt" />
+                                <label htmlFor="quarta">
+                                    <input type="checkbox" id="quarta" className="checkOpt" name="quarta" onChange={props.handleChange}/>
                                     Quarta
                                 </label>
                             </div>
                             <div className="check-option">
-                                <label>
-                                    <input type="checkbox" className="checkOpt" />
+                                <label htmlFor="quinta">
+                                    <input type="checkbox" id="quinta" className="checkOpt" name="quinta" onChange={props.handleChange}/>
                                     Quinta
                                 </label>
                             </div>
                             <div className="check-option">
-                                <label>
-                                    <input type="checkbox" className="checkOpt" />
+                            <label htmlFor="sexta">
+                                    <input type="checkbox" id="sexta" className="checkOpt" name="sexta" onChange={props.handleChange}/>
                                     Sexta
                                 </label>
                             </div>
                             <div className="check-option">
-                                <label>
-                                    <input type="checkbox" className="checkOpt" />
+                                <label htmlFor="sabado">
+                                    <input type="checkbox"id="sabado" className="checkOpt" name="sabado" onChange={props.handleChange}/>
                                     Sábado
                                 </label>
                             </div>
                         </div>
                     </div>
                 </div>
-                <button type="button" class="btn btn-primary">OK</button>
+                <button type="submit" className="btn btn-primary">OK</button>
             </form>
         </div>
     )
--- src/routes/Rotine/Rotine.js
+++ src/routes/Rotine/Rotine.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import Form from '../../components/Form/Form'
+import {base} from '../../assets/rebase'
 
 class Rotine extends React.Component {
   state = {
+    form: {
+      dias: []
+    }
+  }
+
+  handleSubmit = (event) => {
+    base.push('usuario/rotina', {
+      data : this.state.form,
+      then(resposta){
+        console.log(resposta)
+      }
+    })
+    event.preventDefault()
+  }
+
+  handleChange = (event) => {
+    let form = { ...this.state.form }
+    if (event.target.type == "checkbox")
+      form.dias.push(event.target.name)
+    else
+      form[event.target.name] = event.target.value
+    this.setState({ form })
+    event.preventDefault();
 
   }
 
   render() {
     return (
       <div className='Rotine'>
-        <Form />
+        <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
       </div>
     )
   }
